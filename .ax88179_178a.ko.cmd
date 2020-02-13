58475e447c5d004788eef75e69521a32f9af9ef3
Travis Northrup <travis.northrup@pm.me>
2020-02-13 15:51:11 -0600

    Adding compiled drivers

---
+++ .ax88179_178a.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/AX88179_178A_LINUX_DRIVER_v1.19.0_SOURCE/ax88179_178a.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /root/AX88179_178A_LINUX_DRIVER_v1.19.0_SOURCE/ax88179_178a.ko /root/AX88179_178A_LINUX_DRIVER_v1.19.0_SOURCE/ax88179_178a.o /root/AX88179_178A_LINUX_DRIVER_v1.19.0_SOURCE/ax88179_178a.mod.o ;  true
---
+++ ax88179_178a.mod.c
@@ -0,0 +1,36 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=usbnet,mii";
+
+MODULE_ALIAS("usb:v0B95p1790d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v0B95p178Ad*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v0DF6p0072d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v17EFp304Bd*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v0930p0A13d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v04E8pA100d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v2001p4A00d*dc*dsc*dp*ic*isc*ip*in*");
+MODULE_ALIAS("usb:v0711p0179d*dc*dsc*dp*ic*isc*ip*in*");
+
+MODULE_INFO(srcversion, "8F65E085D3E499D6DC74D3D");
